d8375df37936d54e1c9f6df2db0424c4d9f59644
thirawr <Tyler@wirelessprvnat-172-17-63-196.near.illinois.edu>
2014-09-02 14:59:19 -0500

    Sequential computation w/ upper and lower bound
    Integrated sequential calculation along with individual calculation

--- collatz_conjecture.py
+++ collatz_conjecture.py
@@ -1,27 +1,46 @@
 #!/usr/bin/env python
 # Calculates how many steps it takes to solve the Collatz Conjecture
+# @thirawr
 import sys
 
+def Stepper(n): #Recursion is nice
+	if(n == 1):
+		return 0
+	elif(n%2 == 0):
+		return 1+Stepper(n/2)
+	else:
+		return 1+Stepper((n*3)+1)
+
+def Series(lower, upper): #Wrapper function for series calculation
+	for i in range(lower, upper):
+		print "It took %s steps for %s to reach 1." %(Stepper(i), i)
+
+def quit():
+	print "Another clean finish!"
+	sys.exit()
+
 print "Collatz Conjecture"
-print "This program determines how many steps are required to complete the\n\
-Collatz sequence for any given N."
+print "This program computes how many steps are required to complete the Collatz sequence for any given N."
 
 while True:
-	print "Enter N (Negative to quit): ",
-	orig = int(raw_input())
-	if(orig < 0):
-		sys.exit()
-	n = orig
 
-	def Stepper(n):
-		if(n == 1):
-			return 0
-		elif(n%2 == 0):
-			return 1+Stepper(n/2)
-		else:
-			return 1+Stepper((n*3)+1)
+	print "Compute sequence for a (s)eries or (i)ndividual number? "
+	choice = raw_input().lower()
+	while((choice != 's') and (choice != 'i')):
+		print "Invalid input. Enter \'s\' for series computation or \'i\' for individual computation: "
+		choice = raw_input().lower()
 
-
-	steps = Stepper(n)
-
-	print "It took %s steps for %s to reach 1." % (steps, orig)
+	if(choice == 's'):
+		print "Enter positive lower bound (Negative to quit): ",
+		lower = int(raw_input())
+		print "Enter positive upper bound (Negative to quit): ",
+		upper = int(raw_input())
+		if((lower <= 0) or (upper < 0)): #Crash protection
+			quit()
+		Series(lower, upper)
+	else:
+		print "Enter N (Negative to quit): ",
+		n = int(raw_input())
+		if(n <= 0): #Crash protection
+			quit()
+		print "It took %s steps for %s to reach 1." % (Stepper(n), n)
